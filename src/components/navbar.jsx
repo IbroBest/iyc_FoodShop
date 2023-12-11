633456061b830d3fcaf03f8f3dd7fc9cee980453
IbroBest <144906288+IbroBest@users.noreply.github.com>
2023-12-11 18:30:34 +0500

    sdsad

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,29 @@
-import { Box, Container, Heading, Image, Input } from "@chakra-ui/react";
+import { Box, Container, Heading, Image, Input, Text } from "@chakra-ui/react";
 import React from "react";
-import settingLogo from "../assets/images/settings.png";
+import { SearchIcon } from "@chakra-ui/icons";
+// import {LocationIcon}from '../assets/icons/location.svg'
+
 export const Navbar = () => {
   return (
     <Box bg={"#FFFFFF"}>
       <Container maxW={"1260px"} m={"0 auto"}>
         {/* birinchi bolaki  */}
         <Box my={"17px"}>
-          <Box display={'flex'} alignItems={'center'} gap={'20px'}>
-            <Heading color={"#ff4600"}>Pizza.uz</Heading>
-            <Input w={'430px'} placeholder='find the food '  />
+          <Box display={"flex"} alignItems={"center"}justifyContent={'space-between'}>
+            <Heading color={"#ff4600"}>Fast food.uz</Heading>
+            <Input w={"430px"} placeholder="find the food " />
+            <Box>
+              {/* <Image src={''}/> */}
+              <Text fontSize={'17px'}> uzbekistan</Text>
+            </Box>
+            <Box>
+              {/* <Image src={''}/> */}
+              <Heading fontSize={'17px'}> +998 88-602-47-77</Heading>
+            </Box>
+            <Box>
+              {/* <Image src={''}/> */}
+              <Text fontSize={'17px'}> Вход  /  Регистрация</Text>
+            </Box>
           </Box>
         </Box>
       </Container>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,11 +1,36 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import { Navbar } from '../components/navbar'
+import { Box, Button, Container, HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { Navbar } from "../components/navbar";
+
+const elements = [
+  {id:1, name:'pizza'},
+  {id:2, name:'burgers'},
+  {id:3, name:'hamburgers'},
+  {id:4, name:'donar'},
+  {id:5, name:'lavash'},
+  {id:6, name:'chicken'},
+  {id:7, name:'kartoshka fri'},
+
+]
 
 export const Header = () => {
   return (
     <Box>
-        <Navbar/>
+      <Navbar />
+      <hr />
+      <Container maxW={"1260px"} m={"0 auto"}>
+        <Box mt={'26px'} display={'flex'}justifyContent={'space-between'}>
+          <Button border={'1px solid #220a4e'} bg={'#2E1066'}color={'white'}  _hover={{background:'none',border:'1px solid #2E1066',color:'#1B1D1F' }} padding={'12px 20px'}>Все категории</Button>
+          <Box  display={'flex'} alignItems={'center'}w={'70%'} justifyContent={'space-around'}>
+          {
+            elements.map((item)=>(
+              <Text key={item.id}  _hover={{color:'#ff4600'}}transition={'0.4s'}cursor={'pointer'} color={'#1B1D1F'}fontSize={'16px'}>{item.name}</Text>
+            ))
+          }
+          </Box>
+          <Button padding={'12px 20px'}>Все категории</Button>
+        </Box>
+      </Container>
     </Box>
-  )
-}
+  );
+};
