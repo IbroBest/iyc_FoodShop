dab2375b42de527ce77e0c1099d6ed097a97db0b
IbroBest <144906288+IbroBest@users.noreply.github.com>
2023-12-17 12:56:16 +0500

    vcv

---
+++ src/components/DrawerMenu.jsx
@@ -0,0 +1,42 @@
+import { Box, Button, CloseButton, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Image, Radio, RadioGroup, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import React from 'react'
+import hamburgerIcon from '../assets/icons/hamburberIcon.svg'
+
+export const DrawerMenu = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [placement, setPlacement] = React.useState('right')
+
+
+    const elements = [
+      { id: 1, name: "pizza" },
+      { id: 2, name: "burgers" },
+      { id: 3, name: "hamburgers" },
+      { id: 4, name: "donar" },
+      { id: 5, name: "lavash" },
+      { id: 6, name: "chicken" },
+      { id: 7, name: "kartoshka fri" },
+    ];
+  return (
+    <>
+      <RadioGroup defaultValue={placement} onChange={setPlacement}>
+      </RadioGroup>
+      <Button onClick={onOpen} bg={'black'}_hover={{opacity:'0.8'}}>
+        <Image src={hamburgerIcon}/>
+      </Button>
+      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <CloseButton onClick={onClose}/>
+            <Box display={{base:'flex',lg:"none"}}flexDirection={'column'} alignItems={"center"}w={"70%"}justifyContent={"space-around"}>
+            {elements.map((item) => (
+              <Text key={item.id}_hover={{ color: "#ff4600" }}transition={"0.4s"}cursor={"pointer"}color={"#1B1D1F"}fontSize={"20px"}>{item.name}</Text>
+            ))}
+          </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -8,6 +8,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import React from "react";
+import { DrawerMenu } from "../components/DrawerMenu";
 
 const elements = [
   { id: 1, name: "pizza" },
@@ -29,15 +30,15 @@ export const Header = () => {
             <Input w={"430px"} placeholder="find the food " />
             <Box>
               {/* <Image src={''}/> */}
-              <Text fontSize={"17px"}> uzbekistan</Text>
+              <Text className="phoneNumber" fontSize={"17px"}> uzbekistan</Text>
             </Box>
             <Box>
               {/* <Image src={''}/> */}
-              <Heading fontSize={"17px"}> +998 88-602-47-77</Heading>
+              <Heading className="phoneNumber"  fontSize={"17px"}> +998 88-602-47-77</Heading>
             </Box>
             <Box>
               {/* <Image src={''}/> */}
-              <Text fontSize={"17px"}> Вход  /  Регистрация</Text>
+              <Text display={{base:'none',lg:'flex'}} fontSize={"17px"}> Вход  /  Регистрация</Text>
             </Box>
           </Box>
         </Box>
@@ -46,14 +47,16 @@ export const Header = () => {
         {/* //////// ikkinchi bolak //////// */}
 
         <hr />
-        <Box mt={"26px"} display={"flex"} justifyContent={"space-between"}>
+        <Box  mt={"26px"} display={"flex"} justifyContent={"space-between"} >
+            <DrawerMenu/>
           <Button border={"1px solid #220a4e"}bg={"#2E1066"}color={"white"}_hover={{background: "none",border: "1px solid #2E1066",color: "#1B1D1F",}}padding={"12px 20px"}>Все категории</Button>
-          <Box display={"flex"}alignItems={"center"}w={"70%"}justifyContent={"space-around"}>
+          <Box display={{base:'none',lg:"flex"}}alignItems={"center"}w={"70%"}justifyContent={"space-around"}>
             {elements.map((item) => (
               <Text key={item.id}_hover={{ color: "#ff4600" }}transition={"0.4s"}cursor={"pointer"}color={"#1B1D1F"}fontSize={"16px"}>{item.name}</Text>
             ))}
           </Box>
-          <Button padding={"12px 20px"}>Все категории</Button>
+              
+          <Button display={'flex'} padding={"12px 20px"}>Karzina</Button>
         </Box>
       </Container>
     </Box>
